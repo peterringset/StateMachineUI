b3ce4081de90a3bf1095cc49a2063f594be9e7a4
Peter Ringset <peter.ringset@eggsdesign.com>
2020-01-06 14:05:43 +0100

    Add ActivityIndicator component

---
+++ StateMachineUI/Views/Components/ActivityIndicator.swift
@@ -0,0 +1,31 @@
+//
+//  ActivityIndicator.swift
+//  StateMachineUI
+//
+//  Created by Peter Ringset on 06/01/2020.
+//  Copyright © 2020 Ringset. All rights reserved.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+
+    @Binding var isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+    
+}
+
+struct ActivityIndicator_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityIndicator(isAnimating: .constant(true), style: .medium)
+            .previewLayout(.sizeThatFits)
+    }
+}
