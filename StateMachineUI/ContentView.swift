6191dafda3e2d4dcecc487d5d9ad2eabb9e0a8e2
Peter Ringset <peter.ringset@eggsdesign.com>
2020-01-06 13:59:37 +0100

    Initial Commit (Xcode)

---
+++ StateMachineUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StateMachineUI
+//
+//  Created by Peter Ringset on 06/01/2020.
+//  Copyright © 2020 Ringset. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
